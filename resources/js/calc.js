7d38e745bb7b557f5791d15bb40420ff5de17ac1
long13579113 <stevencao135@gmail.com>
2022-09-10 22:46:42 +0700

    add project

--- resources/js/calc.js
+++ resources/js/calc.js
@@ -1,13 +1,15 @@
- 
-for(let i=0 ; i<=199 ; i++)
-{
-    setTimeout(() => document.getElementById("abc1").innerHTML = i, 6*i);
-}
-for(let i=0 ; i<=575 ; i++)
-{
-    setTimeout(() => document.getElementById("abc2").innerHTML = i, 3*i);
-}
-for(let i=0 ; i<=69 ; i++)
-{
-    setTimeout(() => document.getElementById("abc3").innerHTML = i, 15*i);
+let count = 0;
+function coutUp() {
+  if (count == 0) {
+    count++;
+    for (let i = 0; i <= 199; i++) {
+      setTimeout(() => (document.getElementById("abc1").innerHTML = i), 6 * i);
+    }
+    for (let i = 0; i <= 575; i++) {
+      setTimeout(() => (document.getElementById("abc2").innerHTML = i), 3 * i);
+    }
+    for (let i = 0; i <= 69; i++) {
+      setTimeout(() => (document.getElementById("abc3").innerHTML = i), 15 * i);
+    }
+  }
 }
