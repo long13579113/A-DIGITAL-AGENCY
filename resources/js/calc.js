f98dc1a2eed31706fa42438916985b0dc995bf08
long13579113 <stevencao135@gmail.com>
2022-09-10 22:06:59 +0700

    add project

---
+++ resources/js/calc.js
@@ -0,0 +1,13 @@
+ 
+for(let i=0 ; i<=199 ; i++)
+{
+    setTimeout(() => document.getElementById("abc1").innerHTML = i, 6*i);
+}
+for(let i=0 ; i<=575 ; i++)
+{
+    setTimeout(() => document.getElementById("abc2").innerHTML = i, 3*i);
+}
+for(let i=0 ; i<=69 ; i++)
+{
+    setTimeout(() => document.getElementById("abc3").innerHTML = i, 15*i);
+}
